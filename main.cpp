2b0798b79325406b4fa163a56bc1c86f5413f4d2
JamesBoston08 <115898129+JamesBoston08@users.noreply.github.com>
2022-11-27 21:25:11 +0000

    Finished part1e

--- main.cpp
+++ main.cpp
@@ -100,7 +100,7 @@ struct CarWash //                                   1) define an empty struct fo
 {
     //number of vacuum cleaners                     2) copied and commented-out plain-english property
     int numVacuumCleaners = 3; //                   3) member variables with relevant data types.
-    //number of eco-friendly cleaning supplies      
+    //number of eco-friendly cleaning supplies                                        
     int numEcoFriendlyCleaningSupplies = 20;     
     //stores the amount of water used per week.     
     float waterUsedPerWeek = 200.f;            
@@ -192,34 +192,47 @@ Thing 1) Cookie Shop
 5 properties:
     1) number of staff (int)    
     2) avg. earnings per week (dollars) (float)
-    3) avg. amount of cookie dough used per week (lbs) (float)
-    4) avg. amount of coffee used per week (lbs) (float)
-    5) amount of cash on hand (float)
+    3) avg. amount of cookie dough used per week (lbs.) (float)
+    4) avg. amount of coffee used per week (lbs.) (float)
+    5) amount of cash on hand (dollars) (float)
 3 things it can do:
     1) bake cookies
     2) calculate total charge for customer
-    3) brew coffee
+    3) buy a new oven
 */
 
 struct CookieShop
 {
-//number of staff (int)
-int numberOfStaff = 10;
-//avg.earnings per week (dollars) (float)
-float avgEarningsPerWeek = 1500.50f;
-//avg. amount of cookie dough per week (lbs.) (float)
-float avgCookieDoughPerWeek = 20.5f;
-//avg. amount of coffee per week (lbs.) (float)
-float avgCoffeePerWeek = 17.8f;
-//amount of cash on hand (dollars) (float)
-float cashOnHand = 2447.89f;
+    //number of staff (int)
+    int numberOfStaff = 10;
+    //avg. earnings per week (dollars) (float)
+    float avgEarningsPerWeek = 1500.50f;
+    //avg. amount of cookie dough used per week (lbs.) (float)
+    float avgCookieDoughPerWeek = 20.5f;
+    //avg. amount of coffee used per week (lbs.) (float)
+    float avgCoffeePerWeek = 17.8f;
+    //amount of cash on hand (dollars) (float)
+    float cashOnHand = 2447.89f;
 
-//bake cookies
-void bakeCookies(int amountOfChocolateChips, int numOfEggs = 10);
-//calculate total charge for customer
-float calculateTotalCustomerCharge(float cookiePrice = 1.95f, float coffeePrice = 3.15f);
-//brew coffee
-void brewCoffee(bool coffeeMakerOn = false, int coffeeUsedPerCup = 2);
+    struct Oven
+    {
+        int widthOfOven = 30;
+        int maxTemp = 450;
+        std::string manufacturer = "Maytag";
+        int widthOfOvenWindow = 26;
+        int numOfKnobs = 7;
+
+        void produceHeat(bool isOvenOn);
+        void displayTimer(std::string setTime);
+        void displayTemp(std::string setTemp);
+    };
+    //bake cookies
+    void bakeCookies(bool isPreheatTempReached);
+    //calculate total charge for customer
+    float calculateTotalCustomerCharge(float cookiePrice = 1.95f, float coffeePrice = 
+    3.15f); // returns total cost of items purchased
+    //buy a new oven
+    void buyNewOven(Oven newOven);
 };
 
 /*
@@ -237,23 +250,23 @@ Thing 2) LawnMower
  */
 struct LawnMower
 {
-//amount of gasoline (gallon) (float)
-float amountOfGas = 3.00f;
-//number of wires (int)
-int numOfWires = 20;
-//number of blades (int)
-int numOfBlades = 3; 
-//blade height setting (inches) (float)
-float bladeHeight = 2.5f;
-//rpms of blades (int)
-int rpmsOfBlades = 3000;
+    //amount of gasoline (gallon) (float)
+    float amountOfGas = 3.00f;
+    //number of wires (int)
+    int numOfWires = 20;
+    //number of blades (int)
+    int numOfBlades = 3; 
+    //blade height setting (inches) (float)
+    float bladeHeight = 2.5f;
+    //rpms of blades (int)
+    int rpmsOfBlades = 3000;
 
-//cut grass
-void cutGrass(bool engineOn = false, bool engineHandleBarPressedDown = false);
-//consume gas
-void consumeGas(bool engineOn = false);
-//self propel
-void selfPropel(bool engineOn = false, bool selfPropelHandleBarPressedDown = false);
+    //cut grass
+    void cutGrass(bool engineOn = false, bool engineHandleBarPressedDown = false);
+    //consume gas
+    void consumeGas(bool engineOn = false);
+    //self propel
+    void selfPropel(bool engineOn = false, bool selfPropelHandleBarPressedDown = false);
 };
 /*
 Thing 3) Laptop
@@ -270,29 +283,29 @@ Thing 3) Laptop
  */
 struct Laptop
 {
-//height of tower (inches) (int)
-int towerHeight = 13;
-//number of fans (int)
-int numOfFans = 3;
-//amount of ram (GB)(int)
-int amountOfRam = 16;
-//number of usb ports (int)
-int numOfUSBPorts = 6;
-//number of keys (int)
-int numOfKeys = 120;
+    //height of tower (inches) (int)
+    int towerHeight = 13;
+    //number of fans (int)
+    int numOfFans = 3;
+    //amount of ram (GB)(int)
+    int amountOfRam = 16;
+    //number of usb ports (int)
+    int numOfUSBPorts = 6;
+    //number of keys (int)
+    int numOfKeys = 120;
 
-//display letters typed
-void typeLetter(bool isLetterKeyPressed);
-//multiply numbers
-int productOfNumbers(int num1, int num2);
-//run software
-void runSoftware(bool hasDoubleClickedIcon);
+    //display letters typed
+    void typeLetter(bool isLetterKeyPressed);
+    //multiply numbers
+    int productOfNumbers(int num1, int num2);// returns product of numbers
+    //run software
+    void runSoftware(bool hasDoubleClickedIcon);
 };
 /*
 Thing 4) Nuclear Power Plant
 5 properties:
     1) amount of water used (gallons per mega-watt hr.) (float)
-    2) temp. of water (float)
+    2) temp. of water (celcius) (float)
     3) number of generators (int)
     4) weight of generator (tons) (float)
     5) number of engineers (int)
@@ -303,188 +316,205 @@ Thing 4) Nuclear Power Plant
  */
 struct NuclearPowerPlant
 {
-//amount of water used (gallons per mega-watt hr.) (float)
-float amountOfWaterUsed = 375.5f;
-//temp. of water (celcius) (float)
-float tempOfWater = 284.8f;
-//number of generators (int)
-int numOfGenerators = 3;
-//weight of generator (tons) (float)
-float weightOfGenerators = 389.79f;
-//number of engineers (int)
-int numOfEngineers = 100;
+    //amount of water used (gallons per mega-watt hr.) (float)
+    float amountOfWaterUsed = 375.5f;
+    //temp. of water (celcius) (float)
+    float tempOfWater = 284.8f;
+    //number of generators (int)
+    int numOfGenerators = 3;
+    //weight of generator (tons) (float)
+    float weightOfGenerators = 389.79f;
+    //number of engineers (int)
+    int numOfEngineers = 100;
 
-//produce heat
-//void produceHeat(bool isnuclearChainReactionSuccessful);
-//generate steam
-//void generateSteam( )
-//produce electricity
+    //produce heat
+    void produceHeat(bool isFissionSuccessful);
+    //generate steam
+    void generateSteam(bool tubesReachedNeededTemp);
+    //produce electricity
+    void produceElectricity(bool isGeneratorReceivingSteam);
 };
 /*
 Thing 5) Airplane
 5 properties:
-    1) Number of passenger seats (int)
-    2) Total weight of plane (lbs.) (float)
-    3) Length of plane wings (meters) (float)
-    4) Number of luggage pieces (int)
-    5) Amount of fuel (gallons) (int)
+    1) number of passenger seats (int)
+    2) total weight of plane (lbs.) (float)
+    3) length of plane wings (meters) (float)
+    4) number of luggage pieces (int)
+    5) amount of fuel (gallons) (int)
 3 things it can do:
-    1) Take off and fly
-    2) Land 
-    3) Send data to control tower
+    1) take off and fly
+    2) land 
+    3) send data to control tower
  */
 struct Airplane
 {
-//Number of passenger seats (int)
-int numOfPassengerSeats = 180;
-//Total weight of plane (lbs.) (float)
-float totalWeightOfPlane = 92354.67f;
-//Length of plane wings (meters) (float)
-float lengthOfPlaneWings = 33.5f;
-//Number of luggage pieces (int)
-int numOfLuggagePieces = 225;
-//Amount of fuel (gallons) (int)
-int amountOfFuel = 20000;
+    //number of passenger seats (int)
+    int numOfPassengerSeats = 180;
+    //total weight of plane (lbs.) (float)
+    float totalWeightOfPlane = 92354.67f;
+    //length of plane wings (meters) (float)
+    float lengthOfPlaneWings = 33.5f;
+    //number of luggage pieces (int)
+    int numOfLuggagePieces = 225;
+    //amount of fuel (gallons) (int)
+    int amountOfFuel = 20000;
 
-//Take off
-void takeOff(bool isRunwayClear, bool isTakeOffSpeedReached);
-//Land
-void land(bool isLandingSpeedReached, bool areWheelsDeployed);
-//Send data to control tower
-//void sendDataToContolTower() 
+    struct Cockpit
+    {
+    int numOfSwitches = 30;
+    int numOfDisplays = 8;
+    int numOfFlightControllers = 2;
+    int numOfRudderPedals = 4;
+    int numOfPilots = 2;
+
+    void turnPlane(int degreesTurned);
+    void landingGearRaised(bool isGearLeverInUpPostion);
+    void talkToAirTrafficControl(bool isPushToTalkSwitchPressed);
+    };
+
+    //take off
+    void takeOff(bool isRunwayClear, bool isTakeOffSpeedReached);
+    //land
+    void land(bool isLandingSpeedReached, bool areWheelsDeployed);
+    //send data to control tower
+    void sendDataToContolTower(); 
 };
 /*
 Thing 6) Hangar
 5 properties:
-    1) Size of hangar (feet) (float)
-    2) Number of planes (int)
-    3) Number of maintenance employees (int)
-    4) Number of aircraft jacks (int)
-    5) Max height of maintenance platforms (feet) (int)
+    1) size of hangar (feet) (float)
+    2) number of planes (int)
+    3) number of maintenance employees (int)
+    4) number of aircraft jacks (int)
+    5) max height of maintenance platforms (feet) (int)
 3 things it can do:
-    1) Wash airplanes
-    2) Perform routine maintenance on airplanes
-    3) Install engine
+    1) wash airplanes
+    2) perform routine maintenance on airplanes
+    3) install engine
  */
 struct Hangar
 {
-//Size of hangar (feet) (float)
-float hangarSize = 8000.0f;
-//Number of planes (int)
-int numOfPlanes = 3;
-//Number of maintenance employees (int)
-int numOfMaintenanceEmployees = 100;
-//Number of aircraft jacks (int)
-int numOfAircraftJacks = 12;
-//Max height of maintenance platforms (feet) (int)
-int maxHeightOfMaintenancePlatform = 12;
+    //size of hangar (feet) (float)
+    float hangarSize = 8000.0f;
+    //number of planes (int)
+    int numOfPlanes = 3;
+    //number of maintenance employees (int)
+    int numOfMaintenanceEmployees = 100;
+    //number of aircraft jacks (int)
+    int numOfAircraftJacks = 12;
+    //nax height of maintenance platforms (feet) (int)
+    int maxHeightOfMaintenancePlatform = 12;
 
-//Wash airplanes
-void washPlane(int numOfEmployeesWashing, int hrsNeededToWashPlane);
-//Perform routine maintenance on airplanes
-void routinePlaneMaintenance(int hrsNeededtoPerformMaintenance, int numOfEmployeesPerformingMaintenace);
-//Install engine
-void installEngine(int numOfEmployeesInstalEngine, int hrsNeededToInstallEngine);
+    //wash airplanes
+    void washPlane(int numOfEmployeesWashing, int hrsNeededToWashPlane);
+    //perform routine maintenance on airplanes
+    void routinePlaneMaintenance(int hrsNeededtoPerformMaintenance, int 
+    numOfEmployeesPerformingMaintenace);
+    //install engine
+    void installEngine(int numOfEmployeesInstallEngine, int hrsNeededToInstallEngine);
 };
 /*
 Thing 7) Parking Lot
 5 properties:
-    1) Number of cars (int)
-    2) Number of parking passes (int)
-    3) Size of parking lot (float)
-    4) Number of parking spaces (int)
-    5) Number of parking lot exits (int)
+    1) number of cars (int)
+    2) number of parking passes (int)
+    3) size of parking lot (acres) (float)
+    4) number of parking spaces (int)
+    5) number of parking lot exits (int)
 3 things it can do:
-    1) Park cars
+    1) park cars
     2) purchase parking
-    3) Lift barrier arm for entering cars
+    3) lift barrier arm for entering cars
  */
 struct ParkingLot
 {
-//Number of cars (int)
-int numOfCars = 3000;
-//Number of parking passes (int)
-int numOfParkingPasses = 2687;
-//Size of parking lot (acres)(float)
-float sizeOfParkingLot = 12.3f;
-//Number of parking spaces (int)
-int numOfParkingSpaces = 3500;
-//Number of parking lot exits (int)
-int numOfParkingLotExits = 5;
+    //number of cars (int)
+    int numOfCars = 3000;
+    //number of parking passes (int)
+    int numOfParkingPasses = 2687;
+    //size of parking lot (acres) (float)
+    float sizeOfParkingLot = 12.3f;
+    //number of parking spaces (int)
+    int numOfParkingSpaces = 3500;
+    //number of parking lot exits (int)
+    int numOfParkingLotExits = 5;
 
-//Park cars
-void parkCars(bool isOpenParkingSpace);
-//Purchase parking
-void purchaseParking(int totalCostOfTicket, bool isParkingPaid);
-//Lift barrier arm for entering cars
-void (bool isTicketButtonPressed);
-}
+    //park cars
+    void parkCars(bool isOpenParkingSpace);
+    //purchase parking
+    void purchaseParking(int totalCostOfTicket, bool isParkingPaid);
+    //lift barrier arm for entering cars
+    void liftBarrierArm(bool isTicketButtonPressed);
+};
 /*
 Thing 8) Control Tower
 5 properties:
-    1) Number of windows (int)
-    2) Number of aircaft trafic controllers (int)
-    3) Height of control tower (feet) (float)
-    4) Number of monitors/displays (int)
-    5) Number of radars (int)
+    1) number of windows (int)
+    2) number of aircaft trafic controllers (int)
+    3) height of control tower (feet) (float)
+    4) number of displays (int)
+    5) number of radars (int)
 3 things it can do:
-    1) Provide airplance clearance to move from gate
-    2) Monitor airplane during flight
-    3) Clear airplane for landing
+    1) provide airplance clearance to move from gate
+    2) monitor airplane during flight
+    3) clear airplane for landing
  */
 struct ControlTower
 {
-//Number of windows (int)
-int numOfWindows = 40;
-//Number of aircaft trafic controllers (int)
-int numOfAirTarfficControllers = 8;
-//Height of control tower (feet) (float)
-float controlTowerHeight = 369.5f;
-//Number of monitors/displays (int)
-int numOfDisplays = 50;
-//Number of radars (int)
-int numOfRadars = 2;
+    //number of windows (int)
+    int numOfWindows = 40;
+    //number of aircaft trafic controllers (int)
+    int numOfAirTarfficControllers = 8;
+    //height of control tower (feet) (float)
+    float controlTowerHeight = 369.5f;
+    //number of displays (int)
+    int numOfDisplays = 50;
+    //number of radars (int)
+    int numOfRadars = 2;
 
-//Provide airplance clearance to move from gate
-void directPlaneOnGround(bool hasSentPushBackClearence, int timeToPushBackFromGate);
-//Monitor airplane during flight 
-void monitorPlaneFlight(float planeSpeed, float planeAttitude, std::string directionOfMovement);
-//Clear airplane for landing
-void clearPlaneForLanding(bool hasSentLandingClearance, bool hasReachedLandingSpeed);
+    //provide airplance clearance to move from gate
+    void provideAirplaneGateClearance(bool hasSentPushBackClearence, int timeToPushBackFromGate);
+    //monitor airplane during flight 
+    void monitorPlaneFlight(float planeSpeed, float planeAttitude, std::string 
+    directionOfMovement);
+    //clear airplane for landing
+    void clearPlaneForLanding(bool hasSentLandingClearance, bool hasReachedLandingSpeed);
 };
 /*
 Thing 9) Terminal Building
 5 properties:
-    1) Number of terminals (int)
-    2) Number of airline ticket counters (int)
-    3) Number of TSA emplyees (int)
-    4) Number of security checkpoints (int)
-    5) Number of gates (int)
+    1) number of terminals (int)
+    2) number of airline ticket counters (int)
+    3) number of TSA emplyees (int)
+    4) number of security checkpoints (int)
+    5) number of gates (int)
 3 things it can do:
-    1) Check in baggage
-    2) Purchase tickets
-    3) Screen passengers for security purposes
+    1) check in baggage
+    2) purchase tickets
+    3) screen passengers for security purposes
  */
 struct TerminalBuilding
 {
-//Number of terminals (int)
-int numOfTerminals = 2;
-//Number of airline ticket counters (int)
-int numOfTicketCounters = 15;
-//Number of TSA emplyees (int)
-int numofTSAEmplyees = 100;
-//Number of security checkpoints (int)
-int numOfSecurityCheckpoints = 10;
-//Number of gates (int)
-int numOfGates = 120;
+    //number of terminals (int)
+    int numOfTerminals = 2;
+    //number of airline ticket counters (int)
+    int numOfTicketCounters = 15;
+    //number of TSA emplyees (int)
+    int numofTSAEmplyees = 100;
+    //number of security checkpoints (int)
+    int numOfSecurityCheckpoints = 10;
+    //number of gates (int)
+    int numOfGates = 120;
 
-//Check in baggage
-void checkBaggage(int numOfBags, float costPerBag);
-//Purchase tickets
-void purchaseTicket(float totalCostOfTicket, bool isTicketPaid = false);
-//Screen passengers for security purposes
-void screenPassengersForSecurity(bool didPassengerWalkThorughImageDetector, bool didImageDisplay, bool wasAnythingDetected);
-}
+    //check in baggage
+    void checkBaggage(int numOfBags, float costPerBag);
+    //purchase tickets
+    void purchaseTicket(float totalCostOfTicket, bool isTicketPaid = false);
+    //screen passengers for security purposes
+    void screenPassengersForSecurity(bool didPassengerWalkThorughImageDetector, bool 
+    didImageDisplay, bool wasAnythingDetected);
+};
 /*
 Thing 10) Airport
 5 properties:
@@ -494,15 +524,31 @@ Thing 10) Airport
     4) Control Tower
     5) Terminal Building
 3 things it can do:
-    1) Maintain airplanes
-    2) Transport passenger to destination
-    3) Transport cargo to destination
+    1) open a restaurant
+    2) transport passenger to destination
+    3) buy a radar
  */
-//Airplane
-//Hangar
-//Parking Lot
-//Control Tower
-//Terminal Building
+struct Airport
+{
+    //Airplane
+    Airplane airplane;
+    //Hangar
+    Hangar hangar;
+    //Parking Lot
+    ParkingLot parkingLot;
+    //Control Tower
+    ControlTower controlTower;
+    //Terminal Building
+    TerminalBuilding terminalBuilding;
+
+    //open a restaturant
+    void openRestaurant(int PriceOfRestaurant, int spaceNeededForRestaurant);
+    //transport passenger to destination
+    void passengerReachDestination(std::string passengerTicketInformation, std::string 
+    planeDestination, bool isPassengerOnPlane);
+    //buy a radar
+    void buyRadar(int priceOfRadar);
+};
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
 
